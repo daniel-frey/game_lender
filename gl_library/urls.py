61974d51dd4b6b0a97c2449d9b93316c44efb62d
Hannah Sindorf <hannah@hsindorf.com>
2019-02-06 13:11:49 -0800

    borrowing functionality

--- gl_library/urls.py
+++ gl_library/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
     UserGameCopyList,
     UserGameCopyDetailView,
-    # BorrowConfirmation
+    BorrowConfirmation
 )
 
 urlpatterns = [
+    path('', UserGameCopyList.as_view(), name='own_game_list_view'),
     path('<int:id>', UserGameCopyList.as_view(), name='game_list_view'),
-    path('details/<int:pk>', UserGameCopyDetailView.as_view(), name='game_detail_view')
+    path('details/<int:pk>', UserGameCopyDetailView.as_view(), name='game_detail_view'),
+    path('borrow/<int:pk>', BorrowConfirmation.as_view(), name='borrow_confirmation_view')
 ]
--- gl_library/views.py
+++ gl_library/views.py
@@ -1,9 +1,14 @@
 
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views import View
+from django.contrib import messages
+
 from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
+
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 from .models import Game, UserGameCopy, BorrowEvent
 
@@ -20,13 +25,15 @@ class UserGameCopyList(LoginRequiredMixin, ListView):
     context_object_name = 'games'
     login_url = reverse_lazy('login')
 
-    def get_queryset(self):
-        return UserGameCopy.objects.filter(owner__id=self.kwargs.get('id'))
-
     def get_context_data(self, **kwargs):
+        if self.kwargs.get('id'):
+            user_id = self.kwargs.get('id')
+        else:
+            user_id = self.request.user.id
+        owner = get_object_or_404(User, id=user_id)
         context = super().get_context_data(**kwargs)
-        context['owner'] = User.objects.filter(id=self.kwargs.get('id'))[0]
-        context['games'] = UserGameCopy.objects.filter(owner__id=self.kwargs.get('id'))
+        context['owner'] = owner
+        context['games'] = UserGameCopy.objects.filter(owner=owner)
         return context
 
 
@@ -37,25 +44,56 @@ class UserGameCopyDetailView(LoginRequiredMixin, DetailView):
     """
     template_name = 'user_game_detail.html'
     model = UserGameCopy
-    context_object_name = 'games'
     login_url = reverse_lazy('login')
-
-    def get_queryset(self):
-        return UserGameCopy.objects.filter(id=self.kwargs.get('pk'))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['game'] = UserGameCopy.objects.filter(id=self.kwargs.get('pk'))[0]
+        game = get_object_or_404(UserGameCopy, id=self.kwargs.get('pk'))
+        context['game'] = game
         return context
 
 
-# class BorrowConfirmation(LoginRequiredMixin, View):
-#     """
-#     GET /library/borrow/<id>
-#         checks if game is available, brings up screen asking user for confirm
-#         if they want to borrow.
-#     POST /library/borrow/<id>
-#         checks if game is available and makes request to borrow it
-#     """
-#     template_name = 'borrow_confirmation.html'
-#     login_url = reverse_lazy('login')
+class BorrowConfirmation(LoginRequiredMixin, TemplateView):
+    """
+    GET /library/borrow/<id>
+        checks if game is available, brings up screen asking user for confirm
+        if they want to borrow.
+    POST /library/borrow/<id>
+        checks if game is available and makes request to borrow it
+    """
+    template_name = 'borrow_confirmation.html'
+    login_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        game = get_object_or_404(UserGameCopy, id=self.kwargs.get('pk'))
+        context['game'] = game
+        return context
+
+    def post(self, request, *args, **kwargs):
+        game = get_object_or_404(UserGameCopy, id=self.kwargs.get('pk'))
+        if BorrowEvent.objects.filter(borrower=request.user, status='pending').first():
+            messages.info(request, f'You\'ve already requested to borrow this game.')
+            return redirect('own_game_list_view')
+
+        borrow_event = BorrowEvent(
+            game=game,
+            borrower=request.user,
+        )
+        borrow_event.save()
+
+        messages.info(request, f'You have requested {game.game.title} from {game.owner}\'s library.')
+        return redirect('own_game_list_view')
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    """
+    GET /profile/
+        lists user's own profile:
+            games they've borrowed
+            pending borrows on their games
+            pending borrows they've made
+            last 5 events (not pending)
+    """
+    template_name = 'profile.html'
+    login_url = reverse_lazy('login')
