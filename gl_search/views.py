4192bc6b7f0ea92a4b72f714a2902f4e5240fce3
Vince Masten <vmasten@me.com>
2019-02-05 21:12:20 -0800

    can build but not render context

--- gl_search/views.py
+++ gl_search/views.py
@@ -8,7 +8,7 @@ def search_view(request):
     if 'name' in request.GET:
         name = request.GET['name']
         url = 'https://api-v3.igdb.com/games/'
-        payload = f'fields name, cover.url, rating; where name ~ "{name}";'
+        payload = f'search "{name}";'
         headers = {
             'user-key': os.getenv('API_KEY')
         }
@@ -17,11 +17,19 @@ def search_view(request):
         data = r.json()
 
         if data:
-            context = {
-                'name': data[0]['name'],
-                'cover': data[0]['cover']['url'],
-                'rating': data[0]['rating'],
-            }
+            context = {}
+            for i in range(len(data)):
+                payload = f'fields name, cover.url, rating; where id = {data[i]["id"]};'
+                req = requests.request("GET", url, data=payload, headers=headers)
+                api_data = req.json()
+                context_dict = {
+                    'name': api_data[0]['name'],
+                    'cover': api_data[0]['cover']['url'],
+                    'rating': api_data[0]['rating'],
+                }
+
+                context[i] = context_dict
+            print(context)
             return render(request, 'search.html', context)
 
     return render(request, 'search.html')
