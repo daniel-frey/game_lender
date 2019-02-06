bb051f4e9fd2e70129597c0ecf0572a4517e9a04
Vince Masten <vmasten@me.com>
2019-02-06 13:31:36 -0800

    search functional and returning data to a template

--- gl_search/views.py
+++ gl_search/views.py
@@ -17,18 +17,19 @@ def search_view(request):
         data = r.json()
 
         if data:
-            context = {}
+            context_dict = {}
             for i in range(len(data)):
+                if i > 5:
+                    break
                 payload = f'fields name, cover.url, rating; where id = {data[i]["id"]};'
                 req = requests.request("GET", url, data=payload, headers=headers)
                 api_data = req.json()
-                context_dict = {
+                context_dict[i] = {
                     'name': api_data[0]['name'],
-                    'cover': api_data[0]['cover']['url'],
-                    'rating': api_data[0]['rating'],
+                    'cover': api_data[0].get('cover', {}).get('url'),
+                    'rating': api_data[0].get('rating'),
                 }
-
-                context[i] = context_dict
+            context = {'games': context_dict}
             print(context)
             return render(request, 'search.html', context)
 
