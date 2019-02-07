698e98803f7c7a817c59f36f9b0998b6883a9681
Vince Masten <vmasten@me.com>
2019-02-07 14:20:44 -0800

    second eyes

--- gl_search/views.py
+++ gl_search/views.py
@@ -3,33 +3,89 @@ import requests
 import os
 import json
 
+from gl_library.models import Game, Platform
+
 
 def search_view(request):
+    url = 'https://api-v3.igdb.com/games/'
+    platforms_url = 'https://api-v3.igdb.com/platforms'
+
     if 'name' in request.GET:
         name = request.GET['name']
-        url = 'https://api-v3.igdb.com/games/'
-        payload = f'search "{name}";'
+        search_payload = f'search "{name}";'
         headers = {
             'user-key': os.getenv('API_KEY')
         }
-        r = requests.request("GET", url, data=payload, headers=headers)
+        r = requests.request("GET", url, data=search_payload, headers=headers)
 
         data = r.json()
 
         if data:
             context_dict = {}
+            print('data length:', len(data))
             for i in range(len(data)):
-                if i > 5:
+                if i > 4:
                     break
-                payload = f'fields name, summary, platforms, cover.url, rating; where id = {data[i]["id"]};'
-                req = requests.request("GET", url, data=payload, headers=headers)
-                api_data = req.json()
-                context_dict[i] = {
-                    'name': api_data[0]['name'],
-                    'summary': api_data[0]['summary'],
-                    'cover': api_data[0].get('cover', {}).get('url'),
-                    'rating': api_data[0].get('rating'),
-                }
+
+                new_game = Game.objects.filter(game_id=data[i]["id"]).first()
+                if new_game:
+                    print(new_game.title)
+                    context_dict[i] = {
+                        'name': new_game.title,
+                        'summary': new_game.description,
+                        'cover': new_game.cover_art,
+                        'rating': new_game.aggregate_rating,
+                    }
+
+                else:
+                    game_payload = f'''fields name,
+                                         summary,
+                                         platforms,
+                                         cover.url,
+                                         rating;
+                                         where id = {data[i]["id"]};'''
+
+                    req = requests.request("GET",
+                                           url,
+                                           data=game_payload,
+                                           headers=headers)
+
+                    api_data = req.json()
+
+                    if api_data[0].get('platforms'):
+                        for i in range(len(api_data[0].get('platforms'))):
+                            platform = Platform.objects.filter(igdb_platform_id=api_data[0]['platforms'][i]).first()
+                            if not platform:
+                                platform_payload = f'fields name; where id = {api_data[0]["platforms"][i]};'
+                                new_platform = requests.request("GET", platforms_url, data=platform_payload, headers=headers)
+                                platform_data = new_platform.json()
+
+                                new_db_platform = Platform()
+                                new_db_platform.igdb_platform_id = api_data[0]['platforms'][i]
+                                new_db_platform.platform_name = platform_data[0].get('name')
+                                new_db_platform.save()
+
+                        platforms = []
+                        for i in range(len(api_data[0].get('platforms'))):
+                            game_platform = Platform.objects.filter(igdb_platform_id=api_data[0]['platforms'][i]).first()
+                            platforms.append(game_platform.platform_name)
+
+                    print(platforms)
+                    context_dict[i] = {
+                        'name': api_data[0].get('name'),
+                        'summary': api_data[0].get('summary'),
+                        'cover': api_data[0].get('cover', {}).get('url'),
+                        'rating': api_data[0].get('rating'),
+                    }
+
+                    game = Game()
+                    game.game_id = data[i]['id']
+                    game.title = api_data[0].get('name')
+                    game.description = api_data[0].get('summary')
+                    game.cover_art = api_data[0].get('cover', {}).get('url')
+                    game.aggregate_rating = api_data[0].get('rating')
+                    game.save()
+
             context = {'games': context_dict}
             print(context)
             return render(request, 'search.html', context)
