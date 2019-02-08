a475cf309e6c5c1b729f2901bdb4dcbbaea473b8
Hannah Sindorf <hannah@hsindorf.com>
2019-02-07 20:49:39 -0800

    add random games to homepage

--- gl_backend/views.py
+++ gl_backend/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from gl_library.models import UserGameCopy
+
 
 def home_view(request):
     """ view for homepage """
-    return render(request, 'base/home.html')
+
+    random_game = UserGameCopy.objects.order_by('?').first()
+    recent_games = UserGameCopy.objects.all().order_by('-id')[:10]
+    other_random_games = UserGameCopy.objects.order_by('?')[:10]
+
+    context = {
+        'random_game': random_game,
+        'recent_games': recent_games,
+        'other_random_games': other_random_games
+    }
+
+    return render(request, 'base/home.html', context)
