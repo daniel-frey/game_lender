853b17af4b54d59ae0594d48d583a69d4cc0e55e
Vince Masten <vmasten@me.com>
2019-02-07 21:07:00 -0800

    minor changes

--- gl_messages/urls.py
+++ gl_messages/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import (
     MessageInboxView,
     MessageDetailView,
-    SendMessageView
+    SendMessageView,
 )
 
 urlpatterns = [
     path('', MessageInboxView.as_view(), name='inbox_view'),
     path('details/<int:pk>/', MessageDetailView.as_view(), name='message_detail_view'),
     path('send/', SendMessageView.as_view(), name='send_message_view'),
-    path('send/<int:pk>/', SendMessageView.as_view(), name='reply_message_view')
+    path('send/<int:pk>/', SendMessageView.as_view(), name='reply_message_view'),
 ]
