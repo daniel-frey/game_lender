626c4a3d4a60536afd770ab5e508dbbd39e55ef0
Hannah Sindorf <hannah@hsindorf.com>
2019-02-08 09:06:44 -0800

    fix homepage issue

--- gl_backend/views.py
+++ gl_backend/views.py
@@ -1,20 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from gl_library.models import UserGameCopy
+from gl_library.models import UserGameCopy, Platform
 
 
 def home_view(request):
     """ view for homepage """
 
-    random_game = UserGameCopy.objects.order_by('?').first()
-    recent_games = UserGameCopy.objects.all().order_by('-id')[:10]
-    other_random_games = UserGameCopy.objects.order_by('?')[:10]
+    if UserGameCopy.objects.all().first():
+        random_game = UserGameCopy.objects.order_by('?').first()
+        recent_games = UserGameCopy.objects.all().order_by('-id')[:10]
+        other_random_games = UserGameCopy.objects.order_by('?')[:10]
 
-    context = {
-        'random_game': random_game,
-        'recent_games': recent_games,
-        'other_random_games': other_random_games
-    }
+        context = {
+            'random_game': random_game,
+            'recent_games': recent_games,
+            'other_random_games': other_random_games
+        }
+
+    else:
+        context = {}
 
     return render(request, 'base/home.html', context)
